15991b193a24725225fd195666973813db632a7d
Omar Reinoso <oreinoso@minsait.com>
2023-01-23 18:10:37 +0100

    formulario acabado

--- p4-angular/ej7-1/src/app/signup-form/comparePassword.ts
+++ p4-angular/ej7-1/src/app/signup-form/comparePassword.ts
@@ -1,14 +1,20 @@
 import { FormGroup } from '@angular/forms';
 
 export function comparePassword(controlName: string, matchingControlName: string) {
+
+  // Return null or object with a boolean var = true
   return (formGroup: FormGroup) => {
+
+    // Control vars of both
     const control = formGroup.controls[controlName];
     const matchingControl = formGroup.controls[matchingControlName];
     
+    // Check if the error is caused by this function and return null if it's not
     if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
       return;
     }
     
+    // Check if values are different -> return: error if they are didfferent || null if not
     if (control.value !== matchingControl.value) {
       matchingControl.setErrors({ mustMatch: true });
     } else {
--- p4-angular/ej7-1/src/app/signup-form/signup-form.component.ts
+++ p4-angular/ej7-1/src/app/signup-form/signup-form.component.ts
@@ -11,6 +11,7 @@ import { comparePassword } from './comparePassword';
 export class SignupFormComponent {
 
   public fg: FormGroup;
+  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   public submitted: boolean = false;
 
   constructor(private fb: FormBuilder) {
@@ -18,7 +19,7 @@ export class SignupFormComponent {
       firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
       lastName: ['', [Validators.pattern('[a-zA-Z]+')]],
       nif: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('[0-9]+[a-zA-Z]')]],
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
       phoneNumber: ['', [Validators.minLength(9), Validators.maxLength(9), Validators.pattern('[0-9]+')]],
       password: ['', [Validators.required, Validators.minLength(6), Validators.pattern('.*[0-9].*')]],
       repeatPassword: ['', [Validators.required]],
@@ -36,6 +37,7 @@ export class SignupFormComponent {
 
     if(this.fg.valid) {
       const allData: FormFields = this.fg.value;
+      console.log(allData);
       this.fg.reset();
       this.submitted = false;
     }
